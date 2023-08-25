2f8d06010e3b4eebeca8906d376782eed3630235
Leroy G <liguan698@gmail.com>
2023-08-25 12:47:06 -0700

    change lib folder

---
+++ lib/db/models.py
@@ -0,0 +1,12 @@
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = "user"
+
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    bmi_id = Column(Integer,)
