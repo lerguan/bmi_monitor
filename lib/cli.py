c67f5d78ebc32e50e7866d7347564a45f97f932b
Leroy G <liguan698@gmail.com>
2023-08-26 15:21:39 -0700

    add banner to start

--- lib/cli.py
+++ lib/cli.py
@@ -1,9 +1,11 @@
 #!usr/bin/env python3
-
+from helpers.banner import Banner
 from simple_term_menu import TerminalMenu
 import ipdb
 
+banner=Banner()
 def start():
+    banner.welcome()
     print("Please enter your Name to get start")
     input_first_name = input("First Name: ")
     input_last_name = input("Last Name: ")
