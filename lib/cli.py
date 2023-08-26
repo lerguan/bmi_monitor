05291d589bce993ec148ee83acabbb230420550c
Leroy G <liguan698@gmail.com>
2023-08-26 15:09:49 -0700

    create start function with name input

--- lib/cli.py
+++ lib/cli.py
@@ -1,2 +1,18 @@
 #!usr/bin/env python3
 
+from simple_term_menu import TerminalMenu
+import ipdb
+
+def start():
+    print("Please enter your Name to get start")
+    input_first_name = input("First Name: ")
+    input_last_name = input("Last Name: ")
+
+    handle_name_input(input_first_name, input_last_name)
+    ipdb.set_trace()
+
+    options = [""]
+
+    def handle_name_input(first_name, last_name):
+        pass
+start()
