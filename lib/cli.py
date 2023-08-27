0876aa175ebd55d02b692fc71cb2d9d1409a515d
Leroy G <liguan698@gmail.com>
2023-08-26 19:07:57 -0700

    create new user menu

--- lib/cli.py
+++ lib/cli.py
@@ -20,11 +20,24 @@ class Cli():
         current_user = user.find_or_create_by(first_name, last_name)
 
         if current_user:
-            self.current_user_menu(current_user, first_name, last_name)
+            self.current_user_menu(current_user)
+        else:
+            print("NO user is found! Do you want to create new user profile?")
+            
+            options = ["Yes", "No"]
+            terminal_menu = TerminalMenu(options)
+            menu_entry_index = terminal_menu.show()
+            
+            if options[menu_entry_index] == "Yes":
+                options = ["", "No"]
+                terminal_menu = TerminalMenu(options)
+                menu_entry_index = terminal_menu.show()
+                self.new_user_menu(first_name, last_name)
+
             
 
-    def current_user_menu(self, user, first_name, last_name):
-        print(f"Welcome, {first_name} {last_name}!")
+    def current_user_menu(self, user):
+        print(f"Welcome, {user.first_name} {user.last_name}!")
         options = ["Input New height and weight", "Check BMI", "Exit"]
         terminal_menu = TerminalMenu(options)
         menu_entry_index = terminal_menu.show()
@@ -33,6 +46,7 @@ class Cli():
             weight = float(input("Weight(lb): "))
             height = float(input("Height(ft) "))
             bmi.update_info(user, weight, height)
+            self.current_user_menu(user)
 
         elif options[menu_entry_index] == "Check BMI":
             pass
